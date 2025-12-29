cdc618a1411e1d35edd165ecaeb8c6284fc67446
majun <59632763+majun369@users.noreply.github.com>
2025-12-29 13:48:06 +0800

    Update MSL.sh

--- MSL.sh
+++ MSL.sh
@@ -1,3 +1,3 @@
-python main.py --d_model 16 --fp 1.5 --gpu 0 --input_c 55 --output_c 55 --e_layers 3 --dataset MSL --mode train --data_path MSL --model_save_path checkpoints/MSL --anormly_ratio 1.0
-python main.py --d_model 16 --gpu 0 --input_c 55 --output_c 55 --e_layers 3 --dataset MSL --mode test --data_path MSL --model_save_path checkpoints/MSL --anormly_ratio 1.0
+python main.py --d_model 16 --fp 3.0 --kernel_size 40 --gpu 0 --input_c 55 --output_c 55 --e_layers 3 --dataset MSL --mode train --data_path MSL --model_save_path checkpoints/MSL --anormly_ratio 1.0
+python main.py --d_model 16 --kernel_size 40 --gpu 0 --input_c 55 --output_c 55 --e_layers 3 --dataset MSL --mode test --data_path MSL --model_save_path checkpoints/MSL --anormly_ratio 1.0
 
