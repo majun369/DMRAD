060495e2023d87d208bd9c66848f007e38e3428e
majun <59632763+majun369@users.noreply.github.com>
2025-12-29 13:56:43 +0800

    Update SWAT.sh

--- SWAT.sh
+++ SWAT.sh
@@ -1,3 +1,3 @@
-python main.py --batch_size 256 --win_size 100 --d_model 320 --kernel_size 25 --lr 2e-4 --gpu 0 --input_c 51 --output_c 51 --num_epochs 4 --e_layers 3 --dataset SWAT --mode train --data_path SWAT --model_save_path checkpoints/SWAT --anormly_ratio 1.0
+python main.py --batch_size 256 --win_size 100 --d_model 320 --fp 1.5 --kernel_size 25 --lr 2e-4 --gpu 0 --input_c 51 --output_c 51 --num_epochs 4 --e_layers 3 --dataset SWAT --mode train --data_path SWAT --model_save_path checkpoints/SWAT --anormly_ratio 1.0
 python main.py --batch_size 256 --win_size 100 --d_model 320 --kernel_size 25 --gpu 0 --input_c 51 --output_c 51 --e_layers 3 --k 3 --dataset SWAT --mode test --data_path SWAT --model_save_path checkpoints/SWAT --anormly_ratio 1.0
 
